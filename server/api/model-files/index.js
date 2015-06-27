8d436578db07a8139623430f4c6fd5b9fd23f7b8
Michael Lai <themichaellai@gmail.com>
2015-06-27 14:38:49 -0700

    Save ModelFile model

--- server/api/model-files/index.js
+++ server/api/model-files/index.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var controller = require('./model-files.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
 
 module.exports = router;
--- server/api/model-files/model-files.controller.js
+++ server/api/model-files/model-files.controller.js
@@ -12,7 +12,7 @@ exports.show = function(req, res) {
   res.send(200);
 };
 
-exports.create = function(req, res) {
+exports.create = function(req, res, next) {
   var form = new multiparty.Form({
     uploadDir: './uploads'
   });
@@ -21,6 +21,27 @@ exports.create = function(req, res) {
     if (err) {
       return res.status(500).send(err);
     }
-    return res.send(201);
+    var missingFields = _.filter(['name', 'description'], function(k) {
+      return !(k in fields);
+    });
+    var missingFiles = _.filter(['file'], function(k) {
+      return !(k in files);
+    });
+    if (!_.isEmpty(missingFields) || !_.isEmpty(missingFiles)) {
+      var missing = missingFields.concat(missingFiles);
+      return res.status(400).send({
+        message: 'Missing fields: ' + missing.join(', ')
+      });
+    }
+    var modelFile = new ModelFile({
+      name: fields.name[0],
+      description: fields.description[0],
+      filename: files.file[0] ? files.file[0].path : '',
+      userId: req.user.id,
+    });
+    modelFile.save(function(err) {
+      if (err) res.send(500, err);
+      return res.send(201);
+    });
   });
 };
