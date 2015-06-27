55157b247502547d80cec8a9b8cb5d88ebe2bfa8
Yu Zhou Lee <lee.yuzhou@gmail.com>
2015-06-27 14:51:11 -0700

    Changed file paths on front end

--- client/app/merchant/merchant.js
+++ client/app/merchant/merchant.js
@@ -7,5 +7,10 @@ angular.module('greylock20152App')
         url: '/merchant',
         templateUrl: 'app/merchant/merchant.html',
         controller: 'MerchantCtrl'
-      });
+      })
+      .state('merchant.create', {
+      	url: '/create',
+      	templateUrl: 'app/merchant/create_experiment/merchant_create_experiment.html',
+        controller: 'MerchantCreateCtrl'
+      })
   });
