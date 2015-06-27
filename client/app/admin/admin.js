b8c12873b7d3d195a152059b5fa3a2c510bc9be5
Michael Lai <themichaellai@gmail.com>
2015-06-27 12:28:28 -0700

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('greylock20152App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
