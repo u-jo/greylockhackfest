cfc56463ec148f839918cd6f026a8d53441da861
Michael Lai <themichaellai@gmail.com>
2015-06-28 02:44:11 -0700

    Add public endpoint for download.

--- server/api/variations/index.js
+++ server/api/variations/index.js
@@ -9,6 +9,7 @@ var router = express.Router({mergeParams: true});
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/:id', controller.update);
+router.get('/:id/download', controller.showFile);
 router.get('/:id/zip', auth.attachUser(), controller.showFile);
 router.post('/', auth.isAuthenticated(), controller.create);
 
